23729c5a9fc084ef308231a25bf465f2593fa428
Jehr_ <jehrman27@gmail.com>
2026-08-06 22:54:30 -0400

    home page init

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return (
+    <div className="flex flex-col mx-5">
+      <div className="flex items-center h-10">
+        <div className="h-5 w-5 m-1 bg-lime-600 rounded-md" />
+        <div>Native Patch</div>
+      </div>
+      <div className="w-auto border-b border-zinc-800" />
+    </div>
+  );
+};
